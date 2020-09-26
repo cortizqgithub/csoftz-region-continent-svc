3f43ef38d21379f3a70cab46422203387b73061e
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2020-09-26 18:51:45 -0500

    Add inital files.

---
+++ src/main/java/com/csoftz/region/continent/ContinentApplication.java
@@ -0,0 +1,13 @@
+package com.csoftz.region.continent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinentApplication.class, args);
+    }
+
+}
